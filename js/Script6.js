b578ee1fb6bdb63bde8b64b4624cdb18f15d79e4
jamiejessical <144182973+jamiejessical@users.noreply.github.com>
2023-11-28 23:27:55 -0500

    Add files via upload

---
+++ js/Script6.js
@@ -0,0 +1,20 @@
+function opacityChange1() {
+    var quinoa1Div = document.getElementById ("quinoa1");
+    quinoa1Div.style.display = "block";
+    var grain1Div = document.getElementById ("grain1");
+    grain1Div.style.opacity = "0%"
+}
+
+function opacityChange2() {
+    var quinoa2Div = document.getElementById ("quinoa2");
+    quinoa2Div.style.display = "block";
+    var grain2Div = document.getElementById ("grain2");
+    grain2Div.style.opacity = "0%"
+}
+
+function opacityChange3() {
+    var quinoa3Div = document.getElementById ("quinoa3");
+    quinoa3Div.style.display = "block";
+    var grain3Div = document.getElementById ("grain3");
+    grain3Div.style.opacity = "0%"
+}
